ca5c4e3c1d9acc6612e80da32c48f003f20b23c8
epahomov <pahomov.egor@gmail.com>
2015-03-30 16:00:41 +0300

    new spark version

--- warm_maven.sh
+++ warm_maven.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd /incubator-zeppelin/
-mvn clean package -Pspark-1.2.1 -Dhadoop.version=2.2.0 -Phadoop-2.2 -Pyarn -DskipTests
+mvn clean package -Pspark-1.3 -Dhadoop.version=2.2.0 -Phadoop-2.2 -Pyarn -DskipTests
